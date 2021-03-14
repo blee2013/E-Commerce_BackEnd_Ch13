ba2986c367a6ac69e8a0ed88c358eac800293aa5
Brittany Moes <brittanylee@Brittanys-MBP.eau.wi.charter.com>
2021-03-14 15:43:08 -0500

    finished Routees, models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,24 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey:'category_id',
-});
+Product.belongsTo(Category
+  //  {
+  // foreignKey:'category_id'
+  // }
+);
 
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_name'
-});
+Category.hasMany(Product
+  //  {
+  // foreignKey: 'category_name'
+  // }
+);
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag,
-foreignKey: 'tag_id'
+Product.belongsToMany(Tag, { through: ProductTag
+// foreignKey: 'tag_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag,
-foreignKey: 'tag_id'
+Tag.belongsToMany(Product, { through: ProductTag
+// foreignKey: 'tag_id'
 });
 
 module.exports = {
